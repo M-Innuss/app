7697a064c9ec7897ee9ecb12fe1768749c86c6ad
Martins Innuss <57411010+M-Innuss@users.noreply.github.com>
2021-01-15 15:29:06 +0100

    Update init.go

--- init.go
+++ init.go
@@ -1,26 +1,35 @@
 // DB SETUP
 
+package app
+
+import (
+    "github.com/revel/revel"
+    _ "github.com/lib/pq"
+    "database/sql"
+)
+
 var DB *sql.DB
 
 func InitDB() {
-	connstring := fmt.Sprintf("user=%s password='%s' dbname=%s sslmode=disable", "user", "pass", "database")
+    connstring := fmt.Sprintf("user=%s password='%s' dbname=%s sslmode=disable", "user", "pass", "database")
 
-	var err error
-	DB, err = sql.Open("postgres", connstring)
-	if err != nil {
-		revel.INFO.Println("DB Error", err)
-	}
-	revel.INFO.Println("DB Connected")
+    var err error
+    DB, err = sql.Open("postgres", connstring)
+    if err != nil {
+        revel.INFO.Println("DB Error", err)
+    }
+    revel.INFO.Println("DB Connected")
 }
 
 func init() {
 
-	revel.Filters = []revel.Filter{
-		revel.PanicFilter,    // Recover from panics and display an error page instead.
-		revel.CompressFilter, // Compress the result.
-		revel.ActionInvoker,  // Invoke the action.
-	}
-
-	revel.OnAppStart(InitDB)
-
+    revel.Filters = []revel.Filter{
+        revel.PanicFilter,             // Recover from panics and display an error page instead.
+        ... snipped ...
+        revel.CompressFilter,          // Compress the result.
+        revel.ActionInvoker,           // Invoke the action.
+    }
+    
+    revel.OnAppStart(InitDB)
+    ...
 }
